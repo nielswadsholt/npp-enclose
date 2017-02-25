3cb20836c6bd4d40b81971efab69447f2dd3160e
nielswadsholt <nwadsholt@ymail.com>
2017-02-25 03:49:52 +0100

    Update premove.py

--- premove.py
+++ premove.py
@@ -1,12 +1,15 @@
 selection = editor.getSelText()
 open_chars = '"\'{(['
 close_chars = '"\'})]'
+
 if selection[0] in open_chars and close_chars[open_chars.find(selection[0])] == selection[-1]:
     caret = editor.getCurrentPos()
     anchor = editor.getAnchor()
+    
     if anchor > caret:
         caret, anchor = anchor, caret
+    
     editor.replaceSel(selection[1:-1])
-    editor.setSelection(caret-2, anchor)
+    editor.setSelection(caret - 2, anchor)
 
-# Shortcut: Ctrl+Alt+0
+# Suggested shortcut: Ctrl+Alt+0
