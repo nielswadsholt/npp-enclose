46f1ae620bcfecd42283b16c572d35ec0ee9d540
nielswadsholt <nwadsholt@ymail.com>
2017-02-25 03:48:28 +0100

    Update pquote.py

--- pquote.py
+++ pquote.py
@@ -1,8 +1,10 @@
 caret = editor.getCurrentPos()
 anchor = editor.getAnchor()
+
 if anchor > caret:
     caret, anchor = anchor, caret
-editor.replaceSel('"'+editor.getSelText()+'"')
-editor.setSelection(caret+2, anchor)
 
-# Shortcut: Ctrl+Alt+2
+editor.replaceSel('"' + editor.getSelText() + '"')
+editor.setSelection(caret + 2, anchor)
+
+# Suggested shortcut: Ctrl+Alt+2
