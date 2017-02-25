248e535f6e8655b8588bee64e7079cf1e69c06c8
nielswadsholt <nwadsholt@ymail.com>
2017-02-25 02:39:21 +0100

    Add files via upload

---
+++ premove.py
@@ -0,0 +1,12 @@
+selection = editor.getSelText()
+open_chars = '"\'{(['
+close_chars = '"\'})]'
+if selection[0] in open_chars and close_chars[open_chars.find(selection[0])] == selection[-1]:
+    caret = editor.getCurrentPos()
+    anchor = editor.getAnchor()
+    if anchor > caret:
+        caret, anchor = anchor, caret
+    editor.replaceSel(selection[1:-1])
+    editor.setSelection(caret-2, anchor)
+
+# Shortcut: Ctrl+Alt+0
---
+++ psinglequote.py
@@ -0,0 +1,8 @@
+caret = editor.getCurrentPos()
+anchor = editor.getAnchor()
+if anchor > caret:
+    caret, anchor = anchor, caret
+editor.replaceSel("'"+editor.getSelText()+"'")
+editor.setSelection(caret+2, anchor)
+
+# Shortcut: Ctrl+Alt+3
